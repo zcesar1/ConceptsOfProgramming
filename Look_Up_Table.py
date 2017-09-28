687a2905c168b6266ee9a06c6d5ab7a4574ab988
Zoe Cesar <zoe.a.cesar@gmail.com>
2017-09-27 21:43:18 -0400

    Created the dictionary

---
+++ Look_Up_Table.py
@@ -0,0 +1,11 @@
+dict = {0: 'specifications',  1: 'symbol', 2: 'forward', 3: 'references', 4: 'function',
+               5: 'pointer', 6: 'array',  7: 'type', 8: 'struct', 9: 'integer', 10: 'enum', 11: 'global',
+               12: 'declarations', 13: 'implementations', 14: 'main',  15: 'parameters',
+               16: 'constant', 17:'begin', 18: 'endfun',  19: 'if', 20: 'then',
+               21: 'else', 22: 'endif', 23: 'repeat', 24: 'until', 25: 'endrepeat',
+               26: 'display', 27: 'set', 28: 'return', 29: 'integer identifier',
+                30: 'float identifier', 31: 'string identifier', 32: 'boolean identifier', 33: 'string literal'}
+
+
+
+print "dict['pointer']: ", dict[5]
