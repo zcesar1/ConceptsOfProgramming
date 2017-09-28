52116df42282485d9bc955b35ded3b32224b20ff
Zoe Cesar <zoe.a.cesar@gmail.com>
2017-09-27 22:45:52 -0400

    fixed the dictionary

--- Look_Up_Table.py
+++ Look_Up_Table.py
@@ -1,11 +1,13 @@
-dict = {0: 'specifications',  1: 'symbol', 2: 'forward', 3: 'references', 4: 'function',
-               5: 'pointer', 6: 'array',  7: 'type', 8: 'struct', 9: 'integer', 10: 'enum', 11: 'global',
-               12: 'declarations', 13: 'implementations', 14: 'main',  15: 'parameters',
-               16: 'constant', 17:'begin', 18: 'endfun',  19: 'if', 20: 'then',
-               21: 'else', 22: 'endif', 23: 'repeat', 24: 'until', 25: 'endrepeat',
-               26: 'display', 27: 'set', 28: 'return', 29: 'integer identifier',
-                30: 'float identifier', 31: 'string identifier', 32: 'boolean identifier', 33: 'string literal'}
+dict = {'specifications': 0, 'symbol': 1, 'forward': 2, 'references': 3, 'function': 4,
+        'pointer': 5, 'array': 6,  'type': 7, 'struct': 8, 'integer': 9, 'enum': 10, 'global': 11,
+        'declarations': 12, 'implementations': 13, 'main': 14,  'parameters': 15,
+        'constant': 16, 'begin': 17, 'endfun': 18,  'if': 19, 'then': 20,
+        'else': 21, 'endif': 22, 'repeat': 23, 'until': 24, 'endrepeat': 25,
+        'display': 26, 'set': 27, 'return': 28, 'integer identifier': 29,
+        'float identifier': 30, 'string identifier': 31, 'boolean identifier': 32, 'string literal': 33}
 
 
 
-print "dict['pointer']: ", dict[5]
+print "dict[5]: ", dict['pointer']
+print dict.keys()
+print dict.items()
