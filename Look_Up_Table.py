36faa9f532dd77e03b9d5d4b97724d82c561a8c2
Zoe Cesar <zoe.a.cesar@gmail.com>
2017-09-29 22:53:15 -0400

    Changed Look up Table class
    Modified the Look up table class to have two seperate dictionaries

--- Look_Up_Table.py
+++ Look_Up_Table.py
@@ -1,13 +1,9 @@
-dict = {'specifications': 0, 'symbol': 1, 'forward': 2, 'references': 3, 'function': 4,
+Keywords = {'specifications': 0, 'symbol': 1, 'forward': 2, 'references': 3, 'function': 4,
         'pointer': 5, 'array': 6,  'type': 7, 'struct': 8, 'integer': 9, 'enum': 10, 'global': 11,
         'declarations': 12, 'implementations': 13, 'main': 14,  'parameters': 15,
         'constant': 16, 'begin': 17, 'endfun': 18,  'if': 19, 'then': 20,
         'else': 21, 'endif': 22, 'repeat': 23, 'until': 24, 'endrepeat': 25,
-        'display': 26, 'set': 27, 'return': 28, 'integer identifier': 29,
-        'float identifier': 30, 'string identifier': 31, 'boolean identifier': 32, 'string literal': 33, 'constant': 34}
+        'display': 26, 'set': 27, 'return': 28}
 
-
-
-print "dict[5]: ", dict['pointer']
-print dict.keys()
-print dict.items()
+Non_Keywords = {'integer identifier': 29, 'float identifier': 30, 'string identifier': 31,
+                'boolean identifier': 32, 'string literal': 33, 'constant': 34}
