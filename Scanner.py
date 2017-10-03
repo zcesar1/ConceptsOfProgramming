102c90b00f34c34a3f8e11b449052ef48c1875c3
Zoe Cesar <zoe.a.cesar@gmail.com>
2017-10-03 18:59:46 -0400

    added a bit to the Scanner
    it's not completely done, but I added comments to help make it clear as
    to what I was trying to do

--- Scanner.py
+++ Scanner.py
@@ -3,7 +3,8 @@ import Look_Up_Table;
 # global variables
 
 file = open("sclex1.scl", 'r')  # opens the file and stores it in a file object that can be manipulated later
-current_token = ();  # tuple, it will store the token number and lexeme
+current_token = ()  # tuple, it will store the token number and lexeme
+current_index_value = ""  # will store the current value that the array is at in the string
 col = 0  # current index in the file as it is being read
 row = 0  # current row in the file as it is being read
 current_line = 0  # current line in the file as it's being read
@@ -29,5 +30,14 @@ print next_line()
 print current_line
 # def get_next_char():  # gets the next token
 
-# def comments(): # interprets whether or not there is a comment, so that it can be ignored
+def comments(): # interprets whether or not there is a comment, so that it can be ignored
+    if current_index_value == "/":
+        if get_next_char() == "*":
+            # keep going until you reach * and the next character is a /
+        if get_next_char() == "/"
+            next_line()
+    else:
+        # it is not a comment so...
+
+
 
