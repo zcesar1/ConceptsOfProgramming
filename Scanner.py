329ba4b76c6d935b7f5417bd3dad3d5b39b01153
Zoe Cesar <zoe.a.cesar@gmail.com>
2017-10-04 18:15:18 -0400

    fixed scanner readline method

--- Scanner.py
+++ Scanner.py
@@ -8,6 +8,7 @@ current_index_value = ""  # will store the current value that the array is at in
 col = 0  # current index in the file as it is being read
 row = 0  # current row in the file as it is being read
 current_line = 0  # current line in the file as it's being read
+file_line = ""
 
 
 # def Scanner(file):
@@ -20,24 +21,22 @@ def next_line():  # gets the next line in the file
         current_line = 1
     else:
         current_line += 1
-    return file.readline()
-# testing next_line()
-print next_line()
-print current_line
-print next_line()
-print current_line
-print next_line()
-print current_line
+    global file_line
+    file_line = file.readline()
+    return file_line
+
+print (file_line, current_line)
+next_line()
+print (file_line, current_line)
+next_line()
+print (file_line, current_line)
 # def get_next_char():  # gets the next token
 
-def comments(): # interprets whether or not there is a comment, so that it can be ignored
-    if current_index_value == "/":
-        if get_next_char() == "*":
+# def comments(): # interprets whether or not there is a comment, so that it can be ignored
+  #  if current_index_value == "/":
+   #     if get_next_char() == "*":
             # keep going until you reach * and the next character is a /
-        if get_next_char() == "/"
-            next_line()
-    else:
-        # it is not a comment so...
-
-
-
+    #    if get_next_char() == "/"
+    #        next_line()
+   # else:
+    #    return
