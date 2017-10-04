e0397c721ea039eedb397e444e29663d9fa9ebc0
Zoe Cesar <zoe.a.cesar@gmail.com>
2017-10-04 19:30:33 -0400

    trying to figure out next char

--- Scanner.py
+++ Scanner.py
@@ -7,7 +7,7 @@ current_token = ()  # tuple, it will store the token number and lexeme
 file_line = ""  # store the current file string being read
 col = 0  # current index in the file as it is being read
 row = 0  # current row in the file as it is being read
-current_index_value = file_line[col]  # will store the current value that the array is at in the string
+current_index_value = file_line  # will store the current value that the array is at in the string
 current_line = 0  # current line in the file as it's being read
 
 
@@ -23,6 +23,8 @@ def next_line():  # gets the next line in the file
         current_line += 1
     global file_line
     file_line = file.readline()
+    global current_index_value
+    current_index_value = file_line[col]
     return file_line
 
 print (file_line)
@@ -31,16 +33,25 @@ print (file_line)
 next_line()
 print (file_line)
 
-print (current_index_value)
+print ("current_index_value", current_index_value)
 
 def get_next_char():  # gets the next token
     global current_index_value
     global col
-    col += 1
-    return current_index_value
-
+    global file_line
+    index = 0
+    while(index < len(file_line)):
+        if index == col:
+            col += 1
+            index += 1
+            return current_index_value
+            break
+        else:
+            return
 get_next_char()
-print (current_index_value)
+print ("next char = ", get_next_char())
+get_next_char()
+print ("next char = ", get_next_char())
 
 # def comments(): # interprets whether or not there is a comment, so that it can be ignored
   #  if current_index_value == "/":
