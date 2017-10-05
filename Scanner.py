9a22b6150b2a3ac3d73794f1020e7dcd3ffc8b1d
Zoe Cesar <zoe.a.cesar@gmail.com>
2017-10-04 22:28:09 -0400

    working on lex method

--- Scanner.py
+++ Scanner.py
@@ -3,7 +3,7 @@ import Look_Up_Table;
 # global variables
 
 file = open("sclex1.scl", 'r')  # opens the file and stores it in a file object that can be manipulated later
-current_token = ()  # tuple, it will store the token number and lexeme
+current_token = []  # list, it will store the token number and lexeme
 file_line = ""  # store the current file string being read
 col = 0  # current index in the file as it is being read
 row = 0  # current row in the file as it is being read
@@ -12,8 +12,19 @@ current_line = 0  # current line in the file as it's being read
 
 # def Scanner(file):
 
-# def lex():  # lexer
-
+def lex():  # lexer
+    global row
+    global col
+    global current_line
+    global current_token
+    global file_line
+    file_line = next_line()
+    if get_next_char() == '':  # skip an empty line
+        next_line()
+        current_line += 1
+        row += 1
+    elif get_next_char() == "/":  # checks for comments
+        comments()
 def next_line():  # gets the next line in the file
     global current_line
     if current_line == 0:
@@ -29,13 +40,20 @@ def get_next_char():  # gets the next token
     col += 1
     return file_line[col]
 
-def comments(file_line): # interprets whether or not there is a comment, so that it can be ignored
+def comments(): # interprets whether or not there is a comment, so that it can be ignored
+    global file_line
+    global row
+    global col
     if file_line[col] == "/":
         if get_next_char() == "*":
             for char in file_line:
                 if char == "*" and get_next_char() == "/":
+                    row += 1
                     return next_line()
         elif get_next_char() == "/":
+            row += 1
             return next_line()
     else:
         return
+
+lex()
