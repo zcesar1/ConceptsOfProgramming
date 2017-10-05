3a91fc8afb4af25d94cdd7a6741cb69b448860c2
Zoe Cesar <zoe.a.cesar@gmail.com>
2017-10-04 22:14:05 -0400

    added code to the comment method

--- Scanner.py
+++ Scanner.py
@@ -23,26 +23,19 @@ def next_line():  # gets the next line in the file
     global file_line
     file_line = file.readline()
     return file_line
-
-print (file_line)
-next_line()
-print (file_line)
-next_line()
-print (file_line)
 def get_next_char():  # gets the next token
     global col
     global file_line
     col += 1
     return file_line[col]
-print ("current line ", file_line[col])
-print ("next char = ", get_next_char())
-print ("next char = ", get_next_char())
 
-# def comments(): # interprets whether or not there is a comment, so that it can be ignored
-  #  if current_index_value == "/":
-   #     if get_next_char() == "*":
-            # keep going until you reach * and the next character is a /
-    #    if get_next_char() == "/"
-    #        next_line()
-   # else:
-    #    return
+def comments(file_line): # interprets whether or not there is a comment, so that it can be ignored
+    if file_line[col] == "/":
+        if get_next_char() == "*":
+            for char in file_line:
+                if char == "*" and get_next_char() == "/":
+                    return next_line()
+        elif get_next_char() == "/":
+            return next_line()
+    else:
+        return
