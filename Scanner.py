05e913acc2b83cb45de9699c7cfa3666d967c2d7
Zoe Cesar <zoe.a.cesar@gmail.com>
2017-10-04 20:01:32 -0400

    fixed get char method

--- Scanner.py
+++ Scanner.py
@@ -7,7 +7,6 @@ current_token = ()  # tuple, it will store the token number and lexeme
 file_line = ""  # store the current file string being read
 col = 0  # current index in the file as it is being read
 row = 0  # current row in the file as it is being read
-current_index_value = file_line  # will store the current value that the array is at in the string
 current_line = 0  # current line in the file as it's being read
 
 
@@ -23,8 +22,6 @@ def next_line():  # gets the next line in the file
         current_line += 1
     global file_line
     file_line = file.readline()
-    global current_index_value
-    current_index_value = file_line[col]
     return file_line
 
 print (file_line)
@@ -32,25 +29,13 @@ next_line()
 print (file_line)
 next_line()
 print (file_line)
-
-print ("current_index_value", current_index_value)
-
 def get_next_char():  # gets the next token
-    global current_index_value
     global col
     global file_line
-    index = 0
-    while(index < len(file_line)):
-        if index == col:
-            col += 1
-            index += 1
-            return current_index_value
-            break
-        else:
-            return
-get_next_char()
+    col += 1
+    return file_line[col]
+print ("current line ", file_line[col])
 print ("next char = ", get_next_char())
-get_next_char()
 print ("next char = ", get_next_char())
 
 # def comments(): # interprets whether or not there is a comment, so that it can be ignored
