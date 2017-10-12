c5b9863f794732b67c978504af336264afadbedc
Zoe Cesar <zoe.a.cesar@gmail.com>
2017-10-11 21:45:51 -0400

    fixing scanner

--- Scanner.py
+++ Scanner.py
@@ -14,25 +14,31 @@ class Lexer:
     dict_value = "" # be the key for the dict values that are operators
 def lex():  # lexer
 
-        global col
-        global row
-        global current_line
-        global current_string
-        global file_line
+        #global col
+        #global row
+        #global current_line
+        #global current_string
+        #global file_line
+
+        col = Lexer.col
+        row = Lexer.row
+        current_string = Lexer.current_string
+        current_line = Lexer.current_line
+        file_line = Lexer.file_line
         with open('sclex1.scl', 'r') as file_line:
             for index in file_line: # index gives the char, while col gives the # that the index is at
-                if index == "":  # skip an empty line
+                #if index == "":  # skip an empty line
                     # next_line()
-                    current_line += 1
-                    row += 1
-                elif index == "/":  # checks for comments
+                #    current_line += 1
+                 #   row += 1
+                if index == "/":  # checks for comments
                     comments()
                 elif re.search(r'[a-zA-Z]', index):  # check for the current char being a character
                     if re.search(r'[a-zA-Z]', get_next_char()):  # check next char for a letter
                         while index != '':
                             current_string = file_line[index]
                         if current_string in Look_Up_Table.Keywords:
-                            global Token
+                            Token = Lexer.Token
                             Token.extend(["Current lexeme", current_string, "Token", Look_Up_Table.Keywords.get(current_string)
                                              , "Value", Look_Up_Table.Keywords[current_string]])
                     elif get_next_char() == '':
@@ -53,7 +59,8 @@ def lex():  # lexer
                     return
             return Token
 def lookup(current_string): # find the corresponding dict values for non-keywords and operators
-    global dict_value
+   # global dict_value
+    dict_value = Lexer.dict_value
     if current_string == '+':
         dict_value = "add_operator"
         return dict_value
@@ -91,22 +98,29 @@ def lookup(current_string): # find the corresponding dict values for non-keyword
         dict_value = "ne_operator"
         return dict_value
 def next_line():  # gets the next line in the file
-        global file_line
-        global current_line
+        #global file_line
+        #global current_line
+        file_line = Lexer.file_line
+        current_line = Lexer.current_line
         file_line.readline()
         current_line += 1
         return file_line
 
 def get_next_char():  # gets the next token
-        global col
-        global file_line
+        #global col
+        #global file_line
+        col = Lexer.col
+        file_line = Lexer.file_line
         col += 1
         return file_line[col]
 
 def comments(): # interprets whether or not there is a comment, so that it can be ignored
-        global file_line
-        global row
-        global col
+       # global file_line
+       # global row
+       # global col
+        col = Lexer.col
+        file_line = Lexer.file_line
+        row = Lexer.row
         if file[col] == "/":
             if get_next_char() == "*":
                 for char in file_line:
